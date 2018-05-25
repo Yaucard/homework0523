8097d7bd21386d3e4b7e92d5793dee0f2a58e6f9
Yaucard <harryand25451@gmail.com>
2018-05-25 14:02:50 +0800

    指向非常數資料的非常數指標

---
+++ Bubble sort1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+void BubbleSort(int *NumBers,int Time)
+{
+    int *Temp=0;
+    int i=0, j=0,a=0;
+    for( i = 0;i<Time; i++) {
+        for( j=i;j< Time;j++) {
+            if( NumBers[j] > NumBers[i] ) 
+            {
+                Temp = NumBers[j];
+                NumBers[j] = NumBers[i];
+                NumBers[i] = Temp;
+            }
+        }
+    }
+  return 0;
+}
